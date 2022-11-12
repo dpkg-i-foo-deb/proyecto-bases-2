16d6a2631283b12c7cb0c59aa2dcdcea17698d0d
Mateo Estrada Ramirez <mateoestradaramirez@outlook.com>
2022-11-11 20:38:44 -0500

    Agregar sinónimos a la base de datos financiera

--- backend/0-creacion/financiera/4 enlaces.sql
+++ backend/0-creacion/financiera/4 enlaces.sql
@@ -5,3 +5,53 @@ CREATE DATABASE LINK enlace_usuarios
 CREATE DATABASE LINK enlace_seguimiento
 	CONNECT TO SEGUIMIENTO IDENTIFIED BY seguimiento
 	USING 'seguimiento:1521/xe';
+
+-- Sinónimos de la base de datos de seguimiento
+
+CREATE OR REPLACE SYNONYM abono FOR 
+	abono@enlace_seguimiento;
+	
+CREATE OR REPLACE SYNONYM solicitud FOR
+	solicitud@enlace_seguimiento;
+
+
+-- Sinónimos de la base de datos de usuarios
+
+CREATE OR REPLACE SYNONYM ciudad FOR 
+	ciudad@enlace_usuarios;
+
+CREATE OR REPLACE SYNONYM cliente FOR
+	cliente@enlace_usuarios;
+
+CREATE OR REPLACE SYNONYM departamento FOR 
+	departamento@enlace_usuarios;
+
+CREATE OR REPLACE SYNONYM empleado FOR
+	empleado@enlace_usuarios;
+
+CREATE OR REPLACE SYNONYM factura FOR
+	factura@enlace_usuarios;
+
+CREATE OR REPLACE SYNONYM pais FOR 
+	pais@enlace_usuarios;
+
+CREATE OR REPLACE SYNONYM persona FOR
+	persona@enlace_usuarios;
+
+CREATE OR REPLACE SYNONYM producto_ofertado FOR
+	producto_ofertado@enlace_usuarios;
+
+CREATE OR REPLACE SYNONYM registro_llamadas FOR
+	registro_llamadas@enlace_usuarios;
+
+CREATE OR REPLACE SYNONYM sucursal FOR
+	sucursal@enlace_usuarios;
+
+CREATE OR REPLACE SYNONYM telefono FOR
+	sucursal@enlace_usuarios;
+
+CREATE OR REPLACE SYNONYM tipo FOR
+	tipo@enlace_usuarios;
+
+CREATE OR REPLACE SYNONYM turno_empleado FOR
+	turno_empleado@enlace_usuarios;
