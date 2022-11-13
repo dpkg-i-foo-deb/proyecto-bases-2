14f2ce8fc4e17478b0d8f5ae807268aa9684bdc3
Mateo Estrada Ramirez <mateoestradaramirez@outlook.com>
2022-11-13 14:28:31 -0500

    Hacer los enlaces de la base de datos financiera públicos

--- backend/0-creacion/financiera/4 enlaces.sql
+++ backend/0-creacion/financiera/4 enlaces.sql
@@ -1,57 +1,57 @@
-CREATE DATABASE LINK enlace_usuarios
+CREATE PUBLIC DATABASE LINK enlace_usuarios
 	CONNECT TO USUARIOS IDENTIFIED BY usuarios
 	USING 'usuarios:1521/xe';
 
-CREATE DATABASE LINK enlace_seguimiento
+CREATE PUBLIC DATABASE LINK enlace_seguimiento
 	CONNECT TO SEGUIMIENTO IDENTIFIED BY seguimiento
 	USING 'seguimiento:1521/xe';
 
 -- Sinónimos de la base de datos de seguimiento
 
-CREATE OR REPLACE SYNONYM abono FOR 
+CREATE OR REPLACE PUBLIC SYNONYM abono FOR 
 	abono@enlace_seguimiento;
 	
-CREATE OR REPLACE SYNONYM solicitud FOR
+CREATE OR REPLACE PUBLIC SYNONYM solicitud FOR
 	solicitud@enlace_seguimiento;
 
 
 -- Sinónimos de la base de datos de usuarios
 
-CREATE OR REPLACE SYNONYM ciudad FOR 
+CREATE OR REPLACE PUBLIC SYNONYM ciudad FOR 
 	ciudad@enlace_usuarios;
 
-CREATE OR REPLACE SYNONYM cliente FOR
+CREATE OR REPLACE PUBLIC SYNONYM cliente FOR
 	cliente@enlace_usuarios;
 
-CREATE OR REPLACE SYNONYM departamento FOR 
+CREATE OR REPLACE PUBLIC SYNONYM departamento FOR 
 	departamento@enlace_usuarios;
 
-CREATE OR REPLACE SYNONYM empleado FOR
+CREATE OR REPLACE PUBLIC SYNONYM empleado FOR
 	empleado@enlace_usuarios;
 
-CREATE OR REPLACE SYNONYM factura FOR
+CREATE OR REPLACE PUBLIC SYNONYM factura FOR
 	factura@enlace_usuarios;
 
-CREATE OR REPLACE SYNONYM pais FOR 
+CREATE OR REPLACE PUBLIC SYNONYM pais FOR 
 	pais@enlace_usuarios;
 
-CREATE OR REPLACE SYNONYM persona FOR
+CREATE OR REPLACE PUBLIC SYNONYM persona FOR
 	persona@enlace_usuarios;
 
-CREATE OR REPLACE SYNONYM producto_ofertado FOR
+CREATE OR REPLACE PUBLIC SYNONYM producto_ofertado FOR
 	producto_ofertado@enlace_usuarios;
 
-CREATE OR REPLACE SYNONYM registro_llamadas FOR
+CREATE OR REPLACE PUBLIC SYNONYM registro_llamadas FOR
 	registro_llamadas@enlace_usuarios;
 
-CREATE OR REPLACE SYNONYM sucursal FOR
+CREATE OR REPLACE PUBLIC SYNONYM sucursal FOR
 	sucursal@enlace_usuarios;
 
-CREATE OR REPLACE SYNONYM telefono FOR
+CREATE OR REPLACE PUBLIC SYNONYM telefono FOR
 	sucursal@enlace_usuarios;
 
-CREATE OR REPLACE SYNONYM tipo FOR
+CREATE OR REPLACE PUBLIC SYNONYM tipo FOR
 	tipo@enlace_usuarios;
 
-CREATE OR REPLACE SYNONYM turno_empleado FOR
+CREATE OR REPLACE PUBLIC SYNONYM turno_empleado FOR
 	turno_empleado@enlace_usuarios;
